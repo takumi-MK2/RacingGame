eefa4f6258ea5e0bd0ffc64a82ee6c3f0ef351a6
MSI\G24001-PC <isshii0807@gmail.com>
2026-05-14 10:24:40 +0900

    ボタンシーンスクリプトいろいろたくさん大盛り上がり

---
+++ racinggame/Assets/Scripts/PlayerDataManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerDataManager : MonoBehaviour
+{
+    public static PlayerDataManager instance;
+
+    [Header("現在の設定人数")]
+    public int playerCount = 2; // デフォルトを2人に設定
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // ボタンから呼ばれる人数セット用メソッド
+    public void SetPlayerCount(int count)
+    {
+        // 2〜4人の範囲に制限（念のため）
+        playerCount = Mathf.Clamp(count, 2, 4);
+        Debug.Log($"<color=cyan>プレイ人数を {playerCount} 人に設定しました！</color>");
+    }
+}
