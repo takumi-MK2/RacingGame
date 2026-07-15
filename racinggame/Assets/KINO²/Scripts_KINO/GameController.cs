41f4ee821324a93ac38e3d168f3151cc08190db0
KinoKyu2525 <hm-g24007@sist.ac.jp>
2026-07-15 14:36:33 +0900

    あ

--- racinggame/Assets/KINO²/Scripts_KINO/GameController.cs
+++ racinggame/Assets/KINO²/Scripts_KINO/GameController.cs
@@ -37,24 +37,11 @@ public class GameController : MonoBehaviour
     // ゲーム経過時間
     float timer = 0;
 
-    private int goalPlayerCount = 0;
+    // プレイヤー
+    [SerializeField] PlayerController player = null;
 
-    // プレイヤー01
-    [SerializeField] PlayerController player01 = null;
-    [SerializeField] private InputManager_AshVP carInputManager01 = null;
-
-    // プレイヤー02
-    [SerializeField] PlayerController02 player02 = null;
-    [SerializeField] private InputManager_AshVP carInputManager02 = null;
-
-    // プレイヤー03
-    [SerializeField] PlayerController03 player03 = null;
-    [SerializeField] private InputManager_AshVP carInputManager03 = null;
-
-    // プレイヤー04
-    [SerializeField] PlayerController04 player04 = null;
-    [SerializeField] private InputManager_AshVP carInputManager04 = null;
-
+    // 車の入力マネージャー
+    [SerializeField] private InputManager_AshVP carInputManager = null;
 
     // ゴール後に表示するボタンの親オブジェクト
     [SerializeField] private GameObject button;
@@ -63,31 +50,18 @@ public class GameController : MonoBehaviour
     {
         CountDownStart();
 
-        // Player1
-        player01.LapEvent.AddListener(OnLap);
-        player01.GoalEvent.AddListener(OnGoalPlayer01);
-
-        // Player2
-        player02.LapEvent.AddListener(OnLap);
-        player02.GoalEvent.AddListener(OnGoalPlayer02);
-
-        // Player3
-        player03.LapEvent.AddListener(OnLap);
-        player03.GoalEvent.AddListener(OnGoalPlayer03);
-
-        // Player4
-        player04.LapEvent.AddListener(OnLap);
-        player04.GoalEvent.AddListener(OnGoalPlayer04);
+        player.LapEvent.AddListener(OnLap);
+        player.GoalEvent.AddListener(OnGoal);
 
         timerText.text = "Time : 000.000 s";
-        OnLap();
+        lapText.text = "1/" + player.GoalLap;
 
+        // 最初はボタンを非表示
         if (button != null)
         {
             button.SetActive(false);
         }
     }
-
 
     void Update()
     {
@@ -121,7 +95,6 @@ public class GameController : MonoBehaviour
         }
     }
 
-
     // カウントダウン開始
     void CountDownStart()
     {
@@ -129,28 +102,18 @@ public class GameController : MonoBehaviour
         SetPlayState(PlayState.Ready);
         countdownText.gameObject.SetActive(true);
 
-        if (carInputManager01 != null) carInputManager01.enabled = false;
-        if (carInputManager02 != null) carInputManager02.enabled = false;
-        if (carInputManager03 != null) carInputManager03.enabled = false;
-        if (carInputManager04 != null) carInputManager04.enabled = false;
+        if (carInputManager != null)
+            carInputManager.enabled = false;
     }
 
     // ゲーム開始
     void StartPlay()
     {
+        Debug.Log("START");
         SetPlayState(PlayState.Play);
 
-        if (carInputManager01 != null)
-            carInputManager01.enabled = true;
-
-        if (carInputManager02 != null)
-            carInputManager02.enabled = true;
-
-        if (carInputManager03 != null)
-            carInputManager03.enabled = true;
-
-        if (carInputManager04 != null)
-            carInputManager04.enabled = true;
+        if (carInputManager != null)
+            carInputManager.enabled = true;
     }
 
     // START表示を消す
@@ -164,69 +127,38 @@ public class GameController : MonoBehaviour
     void SetPlayState(PlayState state)
     {
         CurrentState = state;
-        player01.CurrentState = state;
-        player02.CurrentState = state;
-        player03.CurrentState = state;
-        player04.CurrentState = state;
+        player.CurrentState = state;
     }
 
     // ラップ数更新
     void OnLap()
     {
-        lapText.text = player01.LapCount + "/" + player01.GoalLap;
+        lapText.text = player.LapCount + "/" + player.GoalLap;
     }
 
     // ゴール時
-    void OnGoal(InputManager_AshVP input)
+    void OnGoal()
     {
-        // このプレイヤーだけ停止
-        if (input != null)
-        {
-            input.enabled = false;
+        SetPlayState(PlayState.Finish);
 
-            if (input.carController != null)
+        countdownText.text = "GOAL";
+        countdownText.gameObject.SetActive(true);
+
+        // 車を停止
+        if (carInputManager != null)
+        {
+            carInputManager.enabled = false;
+
+            if (carInputManager.carController != null)
             {
-                input.carController.ProvideInputs(0f, 0f, 0f);
+                carInputManager.carController.ProvideInputs(0f, 0f, 0f);
             }
         }
 
-        // ゴール人数を加算
-        goalPlayerCount++;
-
-        Debug.Log("Goal Player : " + goalPlayerCount);
-
-        // 全員ゴールしたら終了
-        if (goalPlayerCount >= 4)
+        // ボタンを表示
+        if (button != null)
         {
-            SetPlayState(PlayState.Finish);
-
-            countdownText.text = "FINISH";
-            countdownText.gameObject.SetActive(true);
-
-            if (button != null)
-            {
-                button.SetActive(true);
-            }
+            button.SetActive(true);
         }
-    }
-
-    void OnGoalPlayer01()
-    {
-        OnGoal(carInputManager01);
-    }
-
-    void OnGoalPlayer02()
-    {
-        OnGoal(carInputManager02);
-    }
-
-    void OnGoalPlayer03()
-    {
-        OnGoal(carInputManager03);
-    }
-
-    void OnGoalPlayer04()
-    {
-        OnGoal(carInputManager04);
     }
 }
